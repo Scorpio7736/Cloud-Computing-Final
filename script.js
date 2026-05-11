748d4959446847d22b26ae5488437489a5deb7b5
Jack London <Jack@thelondons.com>
2026-05-10 22:12:24 -0500

    Modify applicant details and experience
    Updated applicant data and modified experience for Cole Von Cole.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const applicants = [
     id: 0,
     name: "Cole Von Cole",
     city: "Mother Russia",
-    experience: "12 years",
+    experience: "13 years",
     license: "Class F",
     availability: "You Wish",
     status: "Approved",
